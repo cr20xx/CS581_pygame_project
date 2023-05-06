4ea3d92afc5506987b039aea54cbdef4e3df8d19
cr20xx <cr20xx@gmail.com>
2023-05-06 13:26:11 -0500

    caption icon & player

--- maincode.py
+++ maincode.py
@@ -16,3 +16,14 @@ background = pygame.image.load('background.png')
 # Sound
 mixer.music.load("background.wav")
 mixer.music.play(-1)
+
+# Caption and Icon
+pygame.display.set_caption("Space Invader")
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+
+# Player
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+playerX_change = 0
