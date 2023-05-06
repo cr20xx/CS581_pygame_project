2f4c4e6413c8ca576999790861d4ae3b0bc7760c
cr20xx <cr20xx@gmail.com>
2023-05-06 13:41:00 -0500

    Enemy movement

--- maincode.py
+++ maincode.py
@@ -141,3 +141,13 @@ while running:
     elif playerX >= 736:
         playerX = 736
 
+    # Enemy Movement
+    for i in range(num_of_enemies):
+
+        # Game Over
+        if enemyY[i] > 440:
+            for j in range(num_of_enemies):
+                enemyY[j] = 2000
+            game_over_text()
+            break
+
