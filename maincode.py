b4a63caa0ea8ead2a594d3c2f96e73c11ad34bf5
cr20xx <cr20xx@gmail.com>
2023-05-06 13:25:20 -0500

    background and sound

--- maincode.py
+++ maincode.py
@@ -9,3 +9,10 @@ pygame.init()
 
 # create the screen
 screen = pygame.display.set_mode((800, 600))
+
+# Background
+background = pygame.image.load('background.png')
+
+# Sound
+mixer.music.load("background.wav")
+mixer.music.play(-1)
